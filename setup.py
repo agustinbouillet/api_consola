0a6af4ef7bbe6c4b0a5d55e6975f47709c3ad101
Agustín Bouillet <agustinbouillet@Agustins-MacBook-Pro.local>
2025-03-28 07:18:20 -0300

    v1.0.7

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='api_consola',
-    version='1.0.6',
+    version='1.0.7',
     author='Agustin Bouillet, Horacio Alvarado',
     author_email='bouilleta@jefatura.gob.ar',
     url='https://www.argentina.gob.ar/',
@@ -15,6 +15,11 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     keywords=['push', 'notifications', 'console', 'messages'],
     packages=setuptools.find_packages(),
+    entry_points={
+        'console_scripts': [
+            'api_consola = api_consola.__main__:main',
+        ],
+    },
     classifiers=[
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.10",
@@ -27,8 +32,7 @@ setuptools.setup(
         'firebase-admin==6.7.0',
     ],
     project_urls={
-        "Bug Tracker": "https://github.com/yourusername/yourproject/issues",
-        "Documentation": "https://yourproject.readthedocs.io/en/latest/",
-        "Source Code": "https://github.com/yourusername/yourproject",
+        "Bug Tracker": "https://git.argentina.gob.ar/argentina/consola-notificaciones/api-consola/-/issues",
+        "Source Code": "https://git.argentina.gob.ar/argentina/consola-notificaciones/api-consola",
     },
 )
